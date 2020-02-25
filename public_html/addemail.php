4021ffe4eb822bdcbe597463797fba06d844d61c
JotaErre <rochajotaerre@gmail.com>
2020-02-25 17:33:40 -0300

    tentativa de conexão com o banco local, sem sucesso

--- public_html/addemail.php
+++ public_html/addemail.php
@@ -10,6 +10,15 @@
       $lastname = $_POST['last_name'];
       $email = $_POST['e_mail'];
 
+      $db = mysqli_conect('localhost', 'root', '', 'elvis_store')
+      or die('Erro ao conectar servidor MYQL');
+      $query = "INSERT INTO email_list(first_name, last_name, email)
+      VALUES ('$firstname', '$lastname', '$emai')";
+
+      $result = mysqli_query($db, $query)
+      or die('Erro requisição ao banc de dados');
+       mysqli_close($bd);
+
       echo 'Cadastrado'.'<br/>';
       echo 'Meu nome:'.$firstname.' '.$lastname.'<br/>';
       echo 'Meu e-mail'.$email.'<br/>';
